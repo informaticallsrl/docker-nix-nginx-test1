956b52d5e17d85d4a8a6e9143fe43975becd6e71
Giacomo Rossetto <jackyman_cs4@live.it>
2020-08-19 16:32:14 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,17 @@
-{
-  pkgs
-}:
-  with pkgs;
-  stdenv.mkDerivation {
-    name = "test";
-    buildInputs = [
-      php72
-    ] ++ (with php72Packages; [
-      composer
-    ]);
-    services.nginx.enable = true;
-    services.nginx.virtualHosts."myhost.org" = {
-        addSSL = true;
-        enableACME = true;
-        root = "/usr/share/nginx/html/";
-    };
 
-  }
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+name = "test";
+buildInputs = [
+    php72
+] ++ (with php72Packages; [
+    composer
+]);
+services.nginx.enable = true;
+services.nginx.virtualHosts."myhost.org" = {
+    addSSL = true;
+    enableACME = true;
+    root = "/usr/share/nginx/html/";
+};
+
